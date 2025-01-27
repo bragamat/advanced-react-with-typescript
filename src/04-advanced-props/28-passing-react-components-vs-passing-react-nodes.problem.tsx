0cfcffffb6d0bfc448a3718a9b4ec7ce8c19ca14
bragamat <mateuscbraga@gmail.com>
2025-01-27 11:32:42 -0300

    solution exercise 28

--- src/04-advanced-props/28-passing-react-components-vs-passing-react-nodes.problem.tsx
+++ src/04-advanced-props/28-passing-react-components-vs-passing-react-nodes.problem.tsx
@@ -8,7 +8,7 @@ import { Equal, Expect } from "../helpers/type-utils";
  * find a way to fix them by changing the definition of TableProps.
  */
 interface TableProps {
-  renderRow: React.ReactNode;
+  renderRow: (arg0: number) => React.ReactNode;
 }
 
 const Table = (props: TableProps) => {
